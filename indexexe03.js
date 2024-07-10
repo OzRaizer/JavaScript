d6f0f53a59e885c46046b72fa8448f2ddfba9f78
Raizer <104086188+OzRaizer@users.noreply.github.com>
2024-07-10 20:52:52 -0300

    simulador-obrigatorio

--- indexexe03.js
+++ indexexe03.js
@@ -1,4 +1,4 @@
-function calcularSomaEMedia() {
+/*function calcularSomaEMedia() {
     
     let soma = 0;
     let quantidade = 0;
@@ -24,4 +24,65 @@ function calcularSomaEMedia() {
     }
   }
   
-  calcularSomaEMedia();
+  calcularSomaEMedia(); */
+
+  class Produto {
+    constructor(nome, cor, peso, tipo, preco) {
+      this.nome = nome;
+      this.cor = cor;
+      this.peso = peso;
+      this.tipo = tipo;
+      this.preco = preco;
+    }
+  
+    descricao() {
+      return `Nome: ${this.nome}\nCor: ${this.cor}\nPeso: ${this.peso}kg\nTipo: ${this.tipo}\nPreço: R$${this.preco.toFixed(2)}`;
+    }
+  }
+  class Carrinho {
+    constructor() {
+      this.produtos = [];
+    }
+    
+    adicionarProduto(produto) {
+      this.produtos.push(produto);
+    }
+  
+    removerProduto(nome) {
+      this.produtos = this.produtos.filter(produto => produto.nome !== nome);
+    }
+  
+    totalCarrinho() {
+      return this.produtos.reduce((total, produto) => total + produto.preco, 0);
+    }
+  
+    exibirCarrinho() {
+      if (this.produtos.length === 0) {
+        return "O carrinho está vazio.";
+      }
+  
+      let descricao = "Produtos no carrinho:\n";
+      this.produtos.forEach(produto => {
+        descricao += produto.descricao() + "\n\n";
+      });
+  
+      descricao += `Total: R$${this.totalCarrinho().toFixed(2)}`;
+      return descricao;
+    }
+  }
+  
+  const maca = new Produto('Maçã', 'vermelha', 0.2, 'fruta', 2.50);
+  const alface = new Produto('Alface', 'verde', 0.3, 'verdura', 1.50);
+  const leite = new Produto('Leite', 'branco', 1.0, 'bebida', 4.00);
+  
+  const carrinho = new Carrinho();
+  
+  carrinho.adicionarProduto(maca);
+  carrinho.adicionarProduto(alface);
+  carrinho.adicionarProduto(leite);
+  
+  console.log(carrinho.exibirCarrinho());
+  
+  carrinho.removerProduto('Alface');
+  
+  console.log(carrinho.exibirCarrinho());
